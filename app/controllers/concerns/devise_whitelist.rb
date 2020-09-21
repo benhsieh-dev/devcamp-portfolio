3738069d8246391abd9e6c7712af4701bb58f9c0
Ben Hsieh <bqh5026@gmail.com>
2020-09-21 16:56:18 -0400

    Complete authentication with virtual attrs for user model

--- app/controllers/concerns/devise_whitelist.rb
+++ app/controllers/concerns/devise_whitelist.rb
@@ -1,6 +1,7 @@
 module DeviseWhitelist
     extend ActiveSupport::Concern
 
+    # Jordan has before_filter in the included method that was throwing error to the server suggesting before_action instead
     included do 
         before_action :configure_permitted_parameters, if: :devise_controller?
     end
