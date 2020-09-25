1f96059ba7c0743447de6dcf1e82de7fd74ffae6
Ben Hsieh <bqh5026@gmail.com>
2020-09-25 18:11:16 -0400

    Add guest user for current_user

--- config/application.rb
+++ config/application.rb
@@ -22,9 +22,7 @@ Bundler.require(*Rails.groups)
 module DevcampPortfolio
   class Application < Rails::Application
     config.load_defaults 6.0
-
-
-    
+  
     config.generators.system_tests = nil
   end
 end
